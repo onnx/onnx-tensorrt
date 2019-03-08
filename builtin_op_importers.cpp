9ce420948372df05452e8f401e1aa5cbe1ab5c95
Kevin Chen <45886021+kevinch-nv@users.noreply.github.com>
2019-03-07 18:43:26 -0800

    Disable all unsupported python unit tests. (#132)
    * Setting default serialization to FALSE
    
    * Convert Unsqueeze axis to TRT axis
    
    * Disbaling unsupported tests from backend test

--- builtin_op_importers.cpp
+++ builtin_op_importers.cpp
@@ -2033,12 +2033,6 @@ DEFINE_BUILTIN_OP_IMPORTER(Unsqueeze) {
   int ndim_in = old_shape.nbDims;
   OnnxAttrs attrs(node);
   auto axes = attrs.get<std::vector<int>>("axes");
-  std::set<int> axes_set(axes.begin(), axes.end());
-  int ndim_out = ndim_in + axes_set.size();
-  ASSERT(ndim_out <= nvinfer1::Dims::MAX_DIMS, ErrorCode::kUNSUPPORTED_NODE);
-  nvinfer1::Dims new_shape;
-  new_shape.nbDims = ndim_out;
-
   // If the input was already a tensor, then we're dealing with a TRT shape,
   // so subtract 1 from the axes. Otherwise, this is an ONNX shape.
   if (inputs.at(0).is_tensor())
@@ -2049,6 +2043,12 @@ DEFINE_BUILTIN_OP_IMPORTER(Unsqueeze) {
           --axis;
       }
   }
+
+  std::set<int> axes_set(axes.begin(), axes.end());
+  int ndim_out = ndim_in + axes_set.size();
+  ASSERT(ndim_out <= nvinfer1::Dims::MAX_DIMS, ErrorCode::kUNSUPPORTED_NODE);
+  nvinfer1::Dims new_shape;
+  new_shape.nbDims = ndim_out;
 
   for (int i = 0, j = 0; j < new_shape.nbDims; ++j )
   {
